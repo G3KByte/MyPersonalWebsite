391f71bae75335c39e1e40a206cfd73f85ceb63f
Jacob Hoel <jac_hoel@yahoo.com>
2020-08-09 23:38:45 -0700

    Fixing import errors of old package, and adding gitignore for package-lock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 
-import Container from "@material-ui/core/Container";
-
 import Home from './scenes/Home/Home';
 import About from './scenes/About';
-import NavigationBar from './components/NavigationBar/';
 
 import './App.css';
 
