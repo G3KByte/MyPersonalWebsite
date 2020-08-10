5a79ec6e7eff9f97b48674eeff5276a8b0daac8d
Jacob Hoel <jac_hoel@yahoo.com>
2020-08-10 01:26:55 -0700

    Adding social icons to home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './scenes/Home/Home';
 import About from './scenes/About';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import './App.css';
+
+library.add(fab);
 
 function App() {
     return (
--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -2,14 +2,30 @@ import React from "react";
 
 import styles from "./Introduction.module.css";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function Introduction() {
 
-    return(
+    return (
         <div className={styles.introSection}>
             <p className={styles.introTitle}>
-                Hello! I'm <span className={styles.introTitleName}>Jacob Hoel</span>.<br />
+                Hello! I'm <span className={styles.introTitleName}>Jacob Hoel</span>.<br/>
                 I'm a full-stack engineer.
             </p>
+            <div className={styles.socialsSection}>
+                <a href={"https://www.linkedin.com/in/jacob-hoel-601221179/"}
+                   className={styles.socialLink}
+                   target={"_blank"}
+                >
+                    <FontAwesomeIcon icon={['fab', 'linkedin']}/>
+                </a>
+                <a href={"https://github.com/G3KByte"}
+                   className={styles.socialLink}
+                   target={"_blank"}
+                >
+                    <FontAwesomeIcon icon={['fab', 'github']}/>
+                </a>
+            </div>
         </div>
     );
 }
